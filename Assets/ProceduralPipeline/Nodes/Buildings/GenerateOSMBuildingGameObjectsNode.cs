3f9d8bd51efe49eba6ca895e9f476a33dd2099c9
George Wigley <zk20435@bristol.ac.uk>
2023-02-03 11:50:18 +0000

    Added parent and material to building pipeline

--- Assets/ProceduralPipeline/Nodes/Buildings/GenerateOSMBuildingGameObjectsNode.cs
+++ Assets/ProceduralPipeline/Nodes/Buildings/GenerateOSMBuildingGameObjectsNode.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.EventSystems;
 using UnityEngine;
 using XNode;
 
@@ -8,7 +9,8 @@ using XNode;
 public class GenerateOSMBuildingGameObjectsNode : ExtendedNode {
 
 	[Input] public OSMBuildingData[] buildingData;
-	[Output] public GameObject[] buildingGameObjects;
+    [Input] public Material material;
+    [Output] public GameObject[] buildingGameObjects;
 
     // Return the correct value of an output port when requested
     public override object GetValue(NodePort port) {
@@ -28,28 +30,35 @@ public class GenerateOSMBuildingGameObjectsNode : ExtendedNode {
         // setup outputs
         List<GameObject> gameObjects = new List<GameObject>();
 
+        // create parent game object
+        GameObject buildingsParent = new GameObject();
+
+
+        Material mat = GetInputValue("material", material);
         // iterate through building classes
         foreach (OSMBuildingData building in buildings)
         {
-             gameObjects.Add(CreateGameObjectFromBuildingData(building));
+            gameObjects.Add(CreateGameObjectFromBuildingData(building, buildingsParent.transform, mat));
         }
 
         buildingGameObjects = gameObjects.ToArray();
         callback.Invoke(true);
 	}
 
-    private GameObject CreateGameObjectFromBuildingData(OSMBuildingData buildingData)
+    private GameObject CreateGameObjectFromBuildingData(OSMBuildingData buildingData, Transform parent, Material mat)
     {
         // create new game object
         GameObject temp = new GameObject();
+
+        temp.transform.parent = parent;
         MeshFilter meshFilter = temp.AddComponent<MeshFilter>();
         // triangulate mesh
         Mesh buildingMesh =  WayToMesh.CreateBuilding(buildingData.footprint.ToArray(), buildingData.buildingHeight);
         // set mesh filter
         meshFilter.sharedMesh = buildingMesh;
         // add collider and renderer
-        temp.AddComponent<MeshCollider>();
-        temp.AddComponent<MeshRenderer>();
+        temp.AddComponent<MeshCollider>().sharedMesh = buildingMesh;
+        temp.AddComponent<MeshRenderer>().sharedMaterial = mat;
         // apply transform updates
         temp.transform.position = Vector3.zero;
         return temp;
