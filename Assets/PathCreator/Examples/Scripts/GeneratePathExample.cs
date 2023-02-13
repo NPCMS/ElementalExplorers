cf0f6b261349954a93ce441f4c10b60a03efd0c8
James Millan <nf20792@bristol.ac.uk>
2023-02-13 17:34:54 +0000

    attempting to draw roads thanks to Sebastian

--- Assets/PathCreator/Examples/Scripts/GeneratePathExample.cs
+++ Assets/PathCreator/Examples/Scripts/GeneratePathExample.cs
@@ -13,7 +13,7 @@ namespace PathCreation.Examples {
             if (waypoints.Length > 0) {
                 // Create a new bezier path from the waypoints.
                 BezierPath bezierPath = new BezierPath (waypoints, closedLoop, PathSpace.xyz);
-                GetComponent<PathCreator> ().bezierPath = bezierPath;
+                GetComponent<PathCreator>().bezierPath = bezierPath;
             }
         }
     }
