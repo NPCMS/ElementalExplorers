7fc77c1696b051c08be21ac1640737f729b08bdf
Matthew Swann <cv20549@bristol.ac.uk>
2023-02-15 19:44:07 +0000

    Fixed player wrapper bugs, added single player prefab, players are inactive until activated by wrapper start (stops vr related bugs)

--- Assets/Scripts/Player/MultiPlayerManager.cs
+++ Assets/Scripts/Player/MultiPlayerManager.cs
@@ -7,39 +7,55 @@ using Valve.VR;
 public class MultiPlayerManager : NetworkBehaviour
 {
     // Start is called when a player is spawned in
-    void Start()
+    private void Start()
     {
-        if (!IsOwner)
+        if (IsOwner) // if the player object is to be controlled by the player then enable all controls 
         {
             foreach (var c in gameObject.GetComponentsInChildren<Camera>())
             {
-                c.enabled = false;
+                c.enabled = true;
             }
             foreach (var c in gameObject.GetComponentsInChildren<SteamVR_Behaviour_Pose>())
             {
-                c.enabled = false;
+                c.enabled = true;
             }
             foreach (var c in gameObject.GetComponentsInChildren<TrackedPoseDriver>())
             {
-                c.enabled = false;
+                c.enabled = true;
             }
             foreach (var c in gameObject.GetComponentsInChildren<PlayerVRController>())
             {
-                c.enabled = false;
+                c.enabled = true;
             }
             foreach (var c in gameObject.GetComponentsInChildren<Grapple>())
             {
-                c.enabled = false;
+                c.enabled = true;
             }
             foreach (var c in gameObject.GetComponentsInChildren<UIInteractVR>())
             {
-                c.enabled = false;
+                c.enabled = true;
             }
+            foreach (var c in gameObject.GetComponentsInChildren<LaserPointer>())
+            {
+                c.enabled = true;
+            }
+            EnableChildren(gameObject);
         }
+
         // enable multiplayer transforms
         foreach (var c in gameObject.GetComponentsInChildren<ClientNetworkTransform>())
         {
             c.enabled = true;
+        }
+    }
+
+    private static void EnableChildren(GameObject gameObject)
+    {
+        for (int c = 0; c < gameObject.transform.childCount; c++)
+        {
+            GameObject child = gameObject.transform.GetChild(c).gameObject;
+            child.SetActive(true);
+            EnableChildren(child);
         }
     }
 }
---
+++ Assets/Scripts/Player/SinglePlayerManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SpatialTracking;
+using Valve.VR;
+
+public class SinglePlayerManager : MonoBehaviour
+{
+    // Start is called when a player is spawned in
+    private void Start()
+    {
+        foreach (var c in gameObject.GetComponentsInChildren<Camera>())
+        {
+            c.enabled = true;
+        }
+        foreach (var c in gameObject.GetComponentsInChildren<SteamVR_Behaviour_Pose>())
+        {
+            c.enabled = true;
+        }
+        foreach (var c in gameObject.GetComponentsInChildren<TrackedPoseDriver>())
+        {
+            c.enabled = true;
+        }
+        foreach (var c in gameObject.GetComponentsInChildren<PlayerVRController>())
+        {
+            c.enabled = true;
+        }
+        foreach (var c in gameObject.GetComponentsInChildren<Grapple>())
+        {
+            c.enabled = true;
+        }
+        foreach (var c in gameObject.GetComponentsInChildren<UIInteractVR>())
+        {
+            c.enabled = true;
+        }
+        foreach (var c in gameObject.GetComponentsInChildren<LaserPointer>())
+        {
+            c.enabled = true;
+        }
+        EnableChildren(gameObject);
+    }
+
+    private static void EnableChildren(GameObject gameObject)
+    {
+        for (int c = 0; c < gameObject.transform.childCount; c++)
+        {
+            GameObject child = gameObject.transform.GetChild(c).gameObject;
+            child.SetActive(true);
+            EnableChildren(child);
+        }
+    }
+}
