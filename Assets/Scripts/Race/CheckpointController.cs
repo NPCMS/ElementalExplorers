b85889527c6f166f706066ce8746d55c0e0390c2
matthewjackswann <cv20549@bristol.ac.uk>
2023-02-21 15:59:28 +0000

    Fixed holograms, and changed player line renders to work correctly

--- Assets/Scripts/Race/CheckpointController.cs
+++ Assets/Scripts/Race/CheckpointController.cs
@@ -1,11 +1,28 @@
+using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CheckpointController : MonoBehaviour
 {
     [SerializeField] public int checkpoint;
     [SerializeField] private bool finish;
+    [SerializeField] private Shader shader;
     public RaceController raceController;
     public bool passed;
+    // reference to shader property
+    private static readonly int h = Shader.PropertyToID("_Y_height");
+
+    public void Start()
+    {
+        float height = gameObject.transform.position.y - gameObject.GetComponent<MeshCollider>().bounds.size[0];
+        
+        Material instanceOfCheckpointShader = new Material(shader);
+        instanceOfCheckpointShader.SetFloat(h, height);
+        var render = gameObject.AddComponent<MeshRenderer>();
+        render.material = instanceOfCheckpointShader;
+        render.shadowCastingMode = ShadowCastingMode.Off;
+
+    }
 
     public void PassCheckpoint(float time)
     {
