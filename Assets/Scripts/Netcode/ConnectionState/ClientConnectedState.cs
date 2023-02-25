b0e22240d80fe52a8884f8b550bed3ae92f402dc
Alex Elwood <wh20899@bristol.ac.uk>
2023-02-25 13:52:27 +0000

    Imported all boss room scripts and fixed compilation bugs

---
+++ Assets/Scripts/Netcode/ConnectionState/ClientConnectedState.cs
@@ -0,0 +1,49 @@
+using System;
+using Unity.BossRoom.UnityServices.Lobbies;
+using UnityEngine;
+using VContainer;
+
+namespace Unity.BossRoom.ConnectionManagement
+{
+    /// <summary>
+    /// Connection state corresponding to a connected client. When being disconnected, transitions to the
+    /// ClientReconnecting state if no reason is given, or to the Offline state.
+    /// </summary>
+    class ClientConnectedState : ConnectionState
+    {
+        [Inject]
+        protected LobbyServiceFacade m_LobbyServiceFacade;
+
+        public override void Enter()
+        {
+            if (m_LobbyServiceFacade.CurrentUnityLobby != null)
+            {
+                m_LobbyServiceFacade.BeginTracking();
+            }
+        }
+
+        public override void Exit() { }
+
+        public override void OnClientDisconnect(ulong _)
+        {
+            var disconnectReason = m_ConnectionManager.NetworkManager.DisconnectReason;
+            if (string.IsNullOrEmpty(disconnectReason))
+            {
+                m_ConnectStatusPublisher.Publish(ConnectStatus.Reconnecting);
+                m_ConnectionManager.ChangeState(m_ConnectionManager.m_ClientReconnecting);
+            }
+            else
+            {
+                var connectStatus = JsonUtility.FromJson<ConnectStatus>(disconnectReason);
+                m_ConnectStatusPublisher.Publish(connectStatus);
+                m_ConnectionManager.ChangeState(m_ConnectionManager.m_Offline);
+            }
+        }
+
+        public override void OnUserRequestedShutdown()
+        {
+            m_ConnectStatusPublisher.Publish(ConnectStatus.UserRequestedDisconnect);
+            m_ConnectionManager.ChangeState(m_ConnectionManager.m_Offline);
+        }
+    }
+}
