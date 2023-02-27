4cf628fee774520145c9de5380331141eeab1d77
Imran Zamin Ali <ge20118@bristol.ac.uk>
2023-02-27 17:05:06 +0000

    Tiling possibly working with pipeline (not 100% if water is working

---
+++ Assets/ProceduralPipeline/Nodes/Terrain/TileToBoundingBoxNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class TileToBoundingBoxNode : ExtendedNode
+{
+	[Input] public Vector2Int tileInput;
+	[Input] public float zoomLevel = 14;
+	[Output] public GlobeBoundingBox boundingBox;
+	
+	// Use this for initialization
+	protected override void Init() {
+		base.Init();
+		
+	}
+
+	// Return the correct value of an output port when requested
+	public override object GetValue(NodePort port) 
+	{
+		if (port.fieldName == "boundingBox")
+		{
+			return boundingBox;
+		}
+		return null; // Replace this
+	}
+
+	public override void CalculateOutputs(Action<bool> callback)
+	{
+		boundingBox = TileCreation.GetBoundingBoxFromTile(GetInputValue("tileInput", tileInput), GetInputValue("zoomLevel", zoomLevel));
+		callback.Invoke(true);
+	}
+}
--- Assets/Scripts/Precompute/TileCreation.cs
+++ Assets/Scripts/Precompute/TileCreation.cs
@@ -20,7 +20,7 @@ public class TileCreation : MonoBehaviour
     }
 
     //https://towardsdatascience.com/map-tiles-locating-areas-nested-parent-tiles-coordinates-and-bounding-boxes-e54de570d0bd
-    Vector2Int GetTileFromCoord(double longitude, double latitude, float level = 12)
+    public static Vector2Int GetTileFromCoord(double longitude, double latitude, float level = 12)
     {
         double latRad = latitude * Math.PI / 180.0;
 
@@ -32,24 +32,24 @@ public class TileCreation : MonoBehaviour
         return new Vector2Int(xTile, yTile);
     }
 
-    double LongitudeFromTile(Vector2Int tile, int n)
+    private static double LongitudeFromTile(Vector2Int tile, int n)
     {
         return 360.0 * tile.x / n - 180.0;
     }
 
-    double LatitudeFromTile(Vector2Int tile, int n)
+    private static double LatitudeFromTile(Vector2Int tile, int n)
     {
         double m = Math.PI - 2.0 * Math.PI * tile.y / n;
         return (180.0 / Math.PI) * Math.Atan(0.5 * (Math.Exp(m) - Math.Exp(-m)));
     }
 
-    GlobeBoundingBox GetBoundingBoxFromTile(Vector2Int tile, float level = 12)
+    public static GlobeBoundingBox GetBoundingBoxFromTile(Vector2Int tile, float level = 12)
     {
         int n = (int) Math.Pow(2, level);
         double south = LatitudeFromTile(tile, n);
-        double west = LongitudeFromTile(tile, n);
+        double east = LongitudeFromTile(tile, n);
         double north = LatitudeFromTile(tile + Vector2Int.up, n);
-        double east = LongitudeFromTile(tile + Vector2Int.right, n);
+        double west = LongitudeFromTile(tile + Vector2Int.right, n);
         GlobeBoundingBox box = new GlobeBoundingBox(south, west, north, east);
         return box;
     }
