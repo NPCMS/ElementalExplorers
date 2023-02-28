20f37211fb008da3c43df8b0a766b501b75f4e13
Alex Elwood <wh20899@bristol.ac.uk>
2023-02-28 12:07:14 +0000

    Player adds their data to session manager

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,3 +1,5 @@
+using Unity.BossRoom.ConnectionManagement;
+using Unity.Multiplayer.Samples.BossRoom;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -24,6 +26,27 @@ public class PlayerManager : NetworkBehaviour
                 SpawnPlayerServerRPC(gameObject.GetComponent<NetworkObject>().OwnerClientId);
             }
         };
+    }
+    
+    public override void OnNetworkSpawn()
+    {
+        gameObject.name = "PlayerManager" + OwnerClientId;
+
+        // Note that this is done here on OnNetworkSpawn in case this NetworkBehaviour's properties are accessed
+        // when this element is added to the runtime collection. If this was done in OnEnable() there is a chance
+        // that OwnerClientID could be its default value (0).
+        if (IsServer)
+        {
+            var sessionPlayerData = SessionManager<SessionPlayerData>.Instance.GetPlayerData(OwnerClientId);
+            if (sessionPlayerData.HasValue)
+            {
+                var playerData = sessionPlayerData.Value;
+                if (!playerData.HasCharacterSpawned)
+                {
+                    SessionManager<SessionPlayerData>.Instance.SetPlayerData(OwnerClientId, playerData);
+                }
+            }
+        }
     }
 
     [ServerRpc]
