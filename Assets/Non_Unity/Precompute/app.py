9a7fbe5d1ec95bcfe73864f5db553aa1cdf06778
ImranZaminAli <izaminali@gmail.com>
2023-03-08 18:44:03 +0000

    can load in chunks using busy waiting. yes i know that is bad - ill fix it later

--- Assets/Non_Unity/Precompute/app.py
+++ Assets/Non_Unity/Precompute/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
-    return send_file(filename, as_attachment=True)
+    return send_file(f'chunks/{filename}', as_attachment=True)
 
 if __name__ == '__main__':
     app.run()
--- Assets/Scripts/Precompute/TileRequest.cs
+++ Assets/Scripts/Precompute/TileRequest.cs
@@ -2,29 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class TileRequest : MonoBehaviour
 {
+    private PrecomputeChunk chunk;
+    private string output = "old";
+    private string requestUrl = "http://127.0.0.1:5000/download/";
+    private UnityWebRequestAsyncOperation asyncOperation;
+    
     // Start is called before the first frame update
-    void Start()
+    // void Start()
+    // {
+    //     StartCoroutine(GetRequest(, "info.txt"));
+    //     Debug.Log(Application.persistentDataPath);
+    // }
+
+    public void Start()
     {
-        StartCoroutine(GetRequest("http://127.0.0.1:5000/", "download/info.txt"));
+        StartCoroutine(GetRequest("info.txt"));
+        
+        Debug.Log(output);
+        
     }
 
-    IEnumerator GetRequest(string uri, string filename)
+    public PrecomputeChunk GetChunk(string filename)
     {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri + filename))
+        StartCoroutine(GetRequest(filename));
+
+        return chunk;
+    } 
+
+    IEnumerator GetRequest(string filename)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(requestUrl + filename))
         {
-            yield return webRequest.SendWebRequest();
+            webRequest.SendWebRequest();
+            while (webRequest.result == UnityWebRequest.Result.InProgress)
+            {
+            }
+
             if (webRequest.result != UnityWebRequest.Result.Success)
             {
                 Debug.LogError(webRequest.error);
+                
             }
             else
             {
                 Debug.Log("success");
-                Debug.Log(webRequest.downloadHandler.text);
+                //output = webRequest.downloadHandler.text;
+
+
+                MemoryStream stream = new MemoryStream(webRequest.downloadHandler.data);
+
+                System.Runtime.Serialization.Formatters.Binary.BinaryFormatter bf =
+                    new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                chunk = (PrecomputeChunk)bf.Deserialize(stream);
             }
+
+            yield return null;
         }
+
+        // asyncOperation = UnityWebRequest.Get(requestUrl + filename).SendWebRequest();
+        //
+        // if (asyncOperation.isDone)
+        // {
+        //     Debug.Log(asyncOperation.webRequest.downloadHandler.text);
+        // }
+        // else
+        // {
+        //     Debug.Log("not finished");
+        // }
+
     }
+
+    
+    void Update()
+    {
+        
+    }
+    
 }
