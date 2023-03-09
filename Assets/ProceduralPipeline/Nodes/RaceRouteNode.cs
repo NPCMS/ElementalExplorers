9aa8ae0c615b903da978fc45bacd8b7fea4bfa8a
matthewjackswann <cv20549@bristol.ac.uk>
2023-03-09 12:59:06 +0000

    Started work on race node

---
+++ Assets/ProceduralPipeline/Nodes/RaceRouteNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using XNode;
+
+[CreateNodeMenuAttribute("Race Generator")]
+public class RaceRouteNode : ExtendedNode
+{
+    [InputAttribute] public GlobeBoundingBox boundingBox;
+    [InputAttribute] public int timeout;
+    [InputAttribute] public int maxSize;
+    [InputAttribute] public bool debug;
+    [OutputAttribute] public OSMRoadWay[] wayArray;
+    
+    public override void CalculateOutputs(Action<bool> callback)
+    {
+        throw new NotImplementedException();
+    }
+}
