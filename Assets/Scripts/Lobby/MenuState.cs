d16e06e6425f0e9447ce4b652f091a524be6f0a2
Alex Elwood <wh20899@bristol.ac.uk>
2023-03-13 15:38:29 +0000

    Only host animates and removed start button

--- Assets/Scripts/Lobby/MenuState.cs
+++ Assets/Scripts/Lobby/MenuState.cs
@@ -39,73 +39,79 @@ public class MenuState : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (_connectionManager.m_CurrentState is OfflineState || IsHost)
         {
-            SceneLoaderWrapper.Instance.LoadScene(secondSceneName, true, LoadSceneMode.Additive);
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            SceneLoaderWrapper.Instance.UnloadAdditiveScenes();
-        }
-        else if (Input.GetKeyDown(KeyCode.X))
-        {
-            Debug.Log("Elevator Up");
-            leftElevator.MoveUp();
-            rightElevator.MoveUp();
-        }
-        else if (Input.GetKeyDown(KeyCode.Z))
-        {
-            Debug.Log("Elevator Down");
-            leftElevator.MoveDown();
-            rightElevator.MoveDown();
-        }
-
-        if (_sessionManager.GetConnectedCount() == 2 && !loadedTutorial)
-        {
-            loadedTutorial = true;
-            StartCoroutine(leftElevator.OpenDoors());
-            StartCoroutine(rightElevator.OpenDoors());
-        }
-
-        List<GameObject> leftElevatorPlayers = leftElevator.GetPlayersInElevator();
-        bool hostInLeftElevator = false;
-        foreach (GameObject player in leftElevatorPlayers)
-        {
-            ulong id = player.GetComponentInParent<NetworkObject>().OwnerClientId;
-            if (id == 0)
+            if (Input.GetKeyDown(KeyCode.A))
             {
-                hostInLeftElevator = true;
+                SceneLoaderWrapper.Instance.LoadScene(secondSceneName, true, LoadSceneMode.Additive);
             }
-        }
-        List<GameObject> rightElevatorPlayers = rightElevator.GetPlayersInElevator();
-        bool nonHostInRightElevator = false;
-        foreach (GameObject player in rightElevatorPlayers)
-        {
-            ulong id = player.GetComponentInParent<NetworkObject>().OwnerClientId;
-            if (id != 0)
+            else if (Input.GetKeyDown(KeyCode.D))
             {
-                nonHostInRightElevator = true;
+                SceneLoaderWrapper.Instance.UnloadAdditiveScenes();
             }
-        }
-        if (leftElevatorPlayers.Count == 1 && hostInLeftElevator)
-        {
-            StartCoroutine(leftElevator.CloseDoors());
-            leftReadyToMove = true;
-        }
-        
-        if (rightElevatorPlayers.Count == 1 && nonHostInRightElevator)
-        {
-            StartCoroutine(rightElevator.CloseDoors());
-            rightReadyToMove = true;
-        }
+            else if (Input.GetKeyDown(KeyCode.X))
+            {
+                Debug.Log("Elevator Up");
+                leftElevator.MoveUp();
+                rightElevator.MoveUp();
+            }
+            else if (Input.GetKeyDown(KeyCode.Z))
+            {
+                Debug.Log("Elevator Down");
+                leftElevator.MoveDown();
+                rightElevator.MoveDown();
+            }
 
-        if (leftReadyToMove && rightReadyToMove)
-        {
-            leftElevator.MoveDown();
-            rightElevator.MoveDown();
-            leftReadyToMove = false;
-            rightReadyToMove = false;
-            SceneLoaderWrapper.Instance.LoadScene(secondSceneName, true, LoadSceneMode.Additive);
+            if (_sessionManager.GetConnectedCount() == 2)
+            {
+                loadedTutorial = true;
+                StartCoroutine(leftElevator.OpenDoors());
+                StartCoroutine(rightElevator.OpenDoors());
+            }
+
+            List<GameObject> leftElevatorPlayers = leftElevator.GetPlayersInElevator();
+            bool hostInLeftElevator = false;
+            foreach (GameObject player in leftElevatorPlayers)
+            {
+                ulong id = player.GetComponentInParent<NetworkObject>().OwnerClientId;
+                if (id == 0)
+                {
+                    hostInLeftElevator = true;
+                }
+            }
+
+            List<GameObject> rightElevatorPlayers = rightElevator.GetPlayersInElevator();
+            bool nonHostInRightElevator = false;
+            foreach (GameObject player in rightElevatorPlayers)
+            {
+                ulong id = player.GetComponentInParent<NetworkObject>().OwnerClientId;
+                if (id != 0)
+                {
+                    nonHostInRightElevator = true;
+                }
+            }
+
+            if (leftElevatorPlayers.Count == 1 && hostInLeftElevator)
+            {
+                StartCoroutine(leftElevator.CloseDoors());
+                leftReadyToMove = true;
+            }
+
+            if (rightElevatorPlayers.Count == 1 && nonHostInRightElevator)
+            {
+                StartCoroutine(rightElevator.CloseDoors());
+                rightReadyToMove = true;
+            }
+
+            if (leftReadyToMove && rightReadyToMove && !loadedTutorial)
+            {
+                leftElevator.MoveDown();
+                rightElevator.MoveDown();
+                loadedTutorial = true;
+                leftReadyToMove = false;
+                rightReadyToMove = false;
+                SceneLoaderWrapper.Instance.LoadScene(secondSceneName, true, LoadSceneMode.Additive);
+            }
         }
     }
 
@@ -122,7 +128,7 @@ public class MenuState : NetworkBehaviour
             _mainMenuUI.gameObject.SetActive(false);
             _lobbyMenuUI.gameObject.SetActive(true);
             _loadingUI.SetActive(false);
-            _lobbyMenuUI.SetUI(_connectionManager.joinCode, false, false);
+            _lobbyMenuUI.SetUI(_connectionManager.joinCode);
         } 
         else if (newState is ClientConnectingState || newState is StartingHostState)
         {
