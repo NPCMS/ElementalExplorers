fe5aa6af8fee4db0674165b7807100118ff3fc37
matthewjackswann <cv20549@bristol.ac.uk>
2023-03-20 14:10:51 +0000

    Changed wait not to be a super class of node

--- Assets/ProceduralPipelineNodes/Async/SyncYeildingNode.cs
+++ Assets/ProceduralPipelineNodes/Async/SyncYeildingNode.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
-
-[NodeTint(0.78f, 0.08f, 0.52f)]
-public abstract class SyncYeildingNode : SyncExtendedNode
+public class SyncYieldingWait
 {
-    [Input] public float minFrameRate;
+    private const float MaxTime = 1f / 60f;
 
     private float lastUpdateTime;
+
+    public SyncYieldingWait()
+    {
+        lastUpdateTime = Time.realtimeSinceStartup;
+    }
 
     protected bool YieldIfTimePassed()
     {
         float currentTime = Time.realtimeSinceStartup;
-        if (currentTime - lastUpdateTime < 1 / minFrameRate) return false;
+        if (currentTime - lastUpdateTime < MaxTime) return false;
         lastUpdateTime = currentTime;
         return true;
     }
