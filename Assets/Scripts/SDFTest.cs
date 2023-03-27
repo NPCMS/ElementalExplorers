eddb839887fb86008d7d1d941ec48d88d49e74a6
Stephen Brock <uq20042@bristol.ac.uk>
2023-03-27 15:48:34 +0100

    Started working on SDF for texture

---
+++ Assets/Scripts/SDFTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SDFTest : MonoBehaviour
+{
+    [SerializeField] private ComputeShader compute;
+    [SerializeField] private Texture2D test;
+    [SerializeField] private Material mat;
+    [SerializeField] private Texture2D output;
+    [SerializeField] private int iterations = 10;
+    [SerializeField] private float amount = 0.5f;
+
+    private void OnValidate()
+    {
+        output = TextureGenerator.RenderSDF(compute, test, iterations, amount);
+        mat.mainTexture = output;
+    }
+}
