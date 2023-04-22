27718e17027210483a58267e4dbcd839f15e3838
Alex Elwood <wh20899@bristol.ac.uk>
2023-04-22 14:56:59 +0100

    Reworked teleport movement

--- Assets/Scripts/Player/TeleportMarkerScript.cs
+++ Assets/Scripts/Player/TeleportMarkerScript.cs
@@ -2,12 +2,9 @@
 
 public class TeleportMarkerScript : MonoBehaviour
 {
-    [SerializeField] private LineRenderer line;
-
     // Update is called once per frame
     void Update()
     {
-        transform.position = line.GetPosition(1) + Vector3.up / 10000;
         transform.rotation = Quaternion.identity;
     }
 }
--- Assets/Scripts/Player/TeleportMovement.cs
+++ Assets/Scripts/Player/TeleportMovement.cs
@@ -6,65 +6,60 @@ public class TeleportMovement : MonoBehaviour
 {
     [SerializeField] private SteamInputCore.Hand hand;
     [SerializeField] private SteamInputCore.Button teleportButton;
-    [SerializeField] private float maxTeleportDistance = 5;
     [SerializeField] private GameObject pointer;
     [SerializeField] private GameObject marker;
     [SerializeField] private LineRenderer parabolaRenderer;
 
     private SteamInputCore.SteamInput steamInput;
     private bool teleportValid;
-    private Vector3 teleportLocation;
-    private int parabolaPoints = 20;
+    private int parabolaPoints;
+    private Vector3[] linePositions = new Vector3[0];
+    
+    [SerializeField] private int maxParabolaPoints = 100;
+    [SerializeField] private float velocity = 2f;
+    [SerializeField] private float maxSegmentDistance = 0.2f;
+    [SerializeField] private float lerpAmount;
 
     private void Start()
     {
         steamInput = SteamInputCore.GetInput();
-        parabolaRenderer.positionCount = parabolaPoints;
     }
 
     void Update()
     {
+        // When button released execute the teleport
+        if (steamInput.GetInputUp(hand, teleportButton) && teleportValid)
+        {
+            ExecuteTeleport(linePositions[parabolaPoints - 1]);
+        }
+        
         // When holding button show if teleport is allowed
         bool buttonDown = steamInput.GetInput(hand, teleportButton);
         if (buttonDown)
         {
-            StartTeleport();
+            linePositions = GravCast(transform.position, Vector3.Lerp(transform.forward,Vector3.up, lerpAmount));
         }
-        DisplayColour(buttonDown);
-        // When button released execute the teleport
-        if (steamInput.GetInputUp(hand, teleportButton))
+        else
         {
-            ExecuteTeleport();
+            parabolaPoints = 0;
         }
+        DisplayTeleportParabola(buttonDown, linePositions);
     }
     private void LateUpdate()
     {
         steamInput.GetInputUp(hand, teleportButton);
     }
 
-    private void StartTeleport()
+    private void DisplayTeleportParabola(bool buttonDown, Vector3[] linePositions)
     {
-        teleportValid = false;
-        // Cast a ray 
-        RaycastHit hit;
-        if (!Physics.Raycast(transform.position, transform.forward, out hit, maxTeleportDistance))
+        parabolaRenderer.positionCount = parabolaPoints;
+        if (buttonDown)
         {
-            return;
-        }
-        if (!ValidateTeleport(hit)) return;
-
-        teleportLocation = hit.point;
-        teleportValid = true;
-    }
-
-    private void DisplayColour(bool buttonDown)
-    {
-        if (buttonDown && teleportValid)
-        {
-            float height = Vector3.Distance(transform.position, teleportLocation) / 4;
-            parabolaRenderer.SetPositions(Parabola(transform.position, teleportLocation, height));
+            parabolaRenderer.SetPositions(linePositions);
             pointer.SetActive(false);
-            marker.SetActive(true);
+            marker.transform.position = linePositions[parabolaPoints - 1];
+            // marker.transform.localRotation = Quaternion.identity;
+            marker.SetActive(teleportValid);
         }
         else
         {
@@ -73,24 +68,46 @@ public class TeleportMovement : MonoBehaviour
         }
     }
 
-    private void ExecuteTeleport()
+    private void ExecuteTeleport(Vector3 teleportLocation)
     {
-        if (teleportValid)
-        {
-            // Move player to the location of the teleport
-            Transform player = gameObject.transform.parent.parent;
-            Vector3 feetPosition = player.position - Vector3.up * player.Find("Body").transform.lossyScale.y;
-            Vector3 translation = teleportLocation - feetPosition;
-            player.position += translation;
-            // Add haptics
-            steamInput.Vibrate(hand, 0.1f, 120, 0.6f);
-        }
+        // Move player to the location of the teleport
+        Transform player = gameObject.transform.parent.parent;
+        Vector3 feetPosition = player.position - Vector3.up * player.Find("Body").transform.lossyScale.y;
+        Vector3 translation = teleportLocation - feetPosition;
+        player.position += translation;
+        // Add haptics
+        steamInput.Vibrate(hand, 0.1f, 120, 0.6f);
     }
 
+    // This function is adapted from: https://answers.unity.com/questions/1464706/is-there-way-to-curve-raycast.html
+    Vector3[] GravCast(Vector3 startPos, Vector3 direction)
+    {
+        RaycastHit hit;
+        Vector3[] vectors = new Vector3[maxParabolaPoints];
+        vectors[0] = startPos;
+        Ray ray = new Ray(startPos, direction);
+        for (int i = 1; i < maxParabolaPoints; i++)
+        {
+            if(Physics.Raycast(ray,out hit, maxSegmentDistance))
+            {
+                teleportValid = ValidateTeleport(hit);
+                parabolaPoints = i;
+                return vectors;
+            }
+            // Debug.DrawRay(ray.origin, ray.direction, Color.blue);
+            ray = new Ray(ray.origin + ray.direction * maxSegmentDistance, ray.direction + Physics.gravity / maxParabolaPoints / velocity);
+            vectors[i] = ray.origin;
+        }
+
+        parabolaPoints = maxParabolaPoints;
+        teleportValid = false;
+        return vectors;
+    }
+    
     private bool ValidateTeleport(RaycastHit hit)
     {
         // If in controlled teleportation scene and
-        // if object is not in teleport layer don't grapple to it
+        // if object is not in teleport layer don't move to it
         string currentScene = SceneManager.GetActiveScene().name;
         if (currentScene is "SpaceshipScene" or "TutorialZone" && hit.transform.gameObject.layer != 10) return false;
 
@@ -100,35 +117,5 @@ public class TeleportMovement : MonoBehaviour
         // Only allow teleports to flat surfaces
         const double flatnessTol = 0.95;
         return !(Vector3.Dot(hit.normal, Vector3.up) < flatnessTol);
-    }
-    
-    // This function is adapted from: https://forum.unity.com/threads/generating-dynamic-parabola.211681/
-    Vector3[] Parabola(Vector3 start, Vector3 end, float height)
-    {
-        Vector3[] points = new Vector3[parabolaPoints];
-        for (int i = 0; i < parabolaPoints; i++)
-        {
-            // Ignore Rider: This type casting is useful
-            float t = (float)i / (float)parabolaPoints;
-            float parabolicT = t * 2 - 1;
-            if (Mathf.Abs(start.y - end.y) < 0.1f) {
-                // start and end are roughly level, pretend they are - simpler solution with less steps
-                Vector3 travelDirection = end - start;
-                Vector3 result = start + t * travelDirection;
-                result.y += (-parabolicT * parabolicT + 1) * height;
-                points[i] = result;
-            } else {
-                // start and end are not level, gets more complicated
-                Vector3 travelDirection = end - start;
-                Vector3 levelDirection = end - new Vector3(start.x, end.y, start.z);
-                Vector3 right = Vector3.Cross(travelDirection, levelDirection);
-                Vector3 up = Vector3.Cross(right, travelDirection);
-                if (end.y > start.y) up = -up;
-                Vector3 result = start + t * travelDirection;
-                result += (-parabolicT * parabolicT + 1) * height * up.normalized;
-                points[i] = result;
-            }
-        }
-        return points;
     }
 }
