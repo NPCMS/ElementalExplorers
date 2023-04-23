ea54295ea19c4d12a13b63579a89913dae068e36
Matthew Swann <cv20549@bristol.ac.uk>
2023-04-23 17:32:08 +0100

    Return to ship sort of done

--- Assets/Scripts/Lobby/EndOfGameReturnTrigger.cs
+++ Assets/Scripts/Lobby/EndOfGameReturnTrigger.cs
@@ -2,18 +2,28 @@ using System;
 using System.Collections.Generic;
 using Netcode.ConnectionManagement;
 using Netcode.ConnectionManagement.ConnectionState;
+using Netcode.SceneManagement;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Netcode.SessionManagement;
 
 public class EndOfGameReturnTrigger : NetworkBehaviour
 {
     
     private List<GameObject> currentCollisions = new();
     private ConnectionManager _connectionManager;
+
+    [SerializeField] private GameObject newPlayer;
     
     private void Awake()
     {
         _connectionManager = FindObjectOfType<ConnectionManager>();
+        SceneManager.sceneLoaded += (_, _) =>
+        {
+            if (!IsHost) return;
+            ReturnToSpaceshipClientRpc();
+        };
     }
     
     private void OnTriggerEnter (Collider col) {
@@ -22,11 +32,29 @@ public class EndOfGameReturnTrigger : NetworkBehaviour
         currentCollisions.Add(col.gameObject);
         if (_connectionManager.m_CurrentState is OfflineState || IsHost && GetPlayersInTeleporter().Count == 2)
         {
-            if (IsHost)
-            {
-                // todo load spaceship scene and reset game / load end of game loop / close doors / other crap
-            }
+            SceneLoaderWrapper.Instance.LoadScene("SpaceshipScene", true, LoadSceneMode.Additive);
         }
+    }
+
+    [ClientRpc]
+    private void ReturnToSpaceshipClientRpc()
+    {
+        if (IsHost)
+        {
+            var p1Pos = GameObject.FindGameObjectWithTag("Player1RespawnPoint").transform.position;
+            MultiPlayerWrapper.localPlayer.ResetPlayerPos();
+            MultiPlayerWrapper.localPlayer.transform.position = p1Pos;
+            MultiPlayerWrapper.localPlayer.GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
+        }
+        else
+        {
+            var p2Pos = GameObject.FindGameObjectWithTag("Player2RespawnPoint").transform.position;
+            MultiPlayerWrapper.localPlayer.ResetPlayerPos();
+            MultiPlayerWrapper.localPlayer.transform.position = p2Pos;
+            MultiPlayerWrapper.localPlayer.GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
+
+        }
+        SceneLoaderWrapper.Instance.UnloadAdditiveScenes();
     }
  
     private void OnTriggerExit (Collider col) {
