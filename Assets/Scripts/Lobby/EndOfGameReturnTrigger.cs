542d599021368a47115c11224e3496748670ed77
James Millan <nf20792@bristol.ac.uk>
2023-04-26 19:55:13 +0100

    Moved return teleportation to spaceship logic

--- Assets/Scripts/Lobby/EndOfGameReturnTrigger.cs
+++ Assets/Scripts/Lobby/EndOfGameReturnTrigger.cs
@@ -10,21 +10,12 @@ using Netcode.SessionManagement;
 
 public class EndOfGameReturnTrigger : NetworkBehaviour
 {
-    
     private List<GameObject> currentCollisions = new();
     private ConnectionManager _connectionManager;
-
-    [SerializeField] private GameObject newPlayer;
-
-    private readonly HashSet<ulong> readyPlayers = new();
-
+    
     private void Awake()
     {
         _connectionManager = FindObjectOfType<ConnectionManager>();
-        SceneManager.sceneLoaded += (_, _) =>
-        {
-            ReturnToSpaceshipServerRpc();
-        };
     }
     
     private void OnTriggerEnter (Collider col) {
@@ -37,36 +28,6 @@ public class EndOfGameReturnTrigger : NetworkBehaviour
         }
     }
 
-    [ServerRpc(RequireOwnership = false)]
-    private void ReturnToSpaceshipServerRpc(ServerRpcParams serverRpcParams = default)
-    {
-        readyPlayers.Add(serverRpcParams.Receive.SenderClientId);
-        if (readyPlayers.Count == 2)
-        {
-            ReturnToSpaceshipClientRpc();
-        }
-    }
-    
-    [ClientRpc]
-    private void ReturnToSpaceshipClientRpc()
-    {
-        if (IsHost)
-        {
-            var p1Pos = GameObject.FindGameObjectWithTag("Player1RespawnPoint").transform.position;
-            MultiPlayerWrapper.localPlayer.ResetPlayerPos();
-            MultiPlayerWrapper.localPlayer.transform.position = p1Pos;
-            MultiPlayerWrapper.localPlayer.GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
-        }
-        else
-        {
-            var p2Pos = GameObject.FindGameObjectWithTag("Player2RespawnPoint").transform.position;
-            MultiPlayerWrapper.localPlayer.ResetPlayerPos();
-            MultiPlayerWrapper.localPlayer.transform.position = p2Pos;
-            MultiPlayerWrapper.localPlayer.GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
-        }
-        SceneLoaderWrapper.Instance.UnloadAdditiveScenes();
-    }
- 
     private void OnTriggerExit (Collider col) {
  
         // Remove the GameObject collided with from the list.
