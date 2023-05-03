445127f0b6b4e41aead60ee22488417c22c8561a
Matthew Swann <cv20549@bristol.ac.uk>
2023-05-03 09:51:36 +0100

    Tried new body rotation

--- Assets/Prefabs/PlayerCharacter/BodyPoseScript.cs
+++ Assets/Prefabs/PlayerCharacter/BodyPoseScript.cs
@@ -17,30 +17,34 @@ public class BodyPoseScript : MonoBehaviour
     [Header("Player Ref")] [SerializeReference]
     private Transform playerRef;
 
-    [Header("Player head offset for hand vectors")] [SerializeField]
-    private float headOffsetScale = 0.2f;
-
     // Update is called once per frame
     void LateUpdate()
     {
         // torso position should be the offset from the head
         gameObject.transform.position = vrCameraRef.position + torsoOffsetFromCamera;
 
-        Vector3 headOffset = playerRef.position - headOffsetScale * vrCameraRef.forward;
-        
         // heuristic for rotation is just average of camera forward and torso to hand vectors
-        Vector3 leftHandVec = leftHand.position - headOffset;
-        Vector3 rightHandVec = rightHand.position - headOffset;
+        Vector3 leftHandVec = leftHand.position - playerRef.position;
+        Vector3 rightHandVec = rightHand.position - playerRef.position;
         Vector3 camForward = vrCameraRef.forward;
 
         Vector2 leftHandXZVec = new Vector2(leftHandVec.x, leftHandVec.z).normalized;
         Vector2 rightHandXZVec = new Vector2(rightHandVec.x, rightHandVec.z).normalized;
         Vector2 camForwardXZVec = new Vector2(camForward.x, camForward.z).normalized;
 
-        if (Vector2.Dot(leftHandVec, camForwardXZVec) < 0 && Vector2.Dot(rightHandXZVec, camForwardXZVec) < 0)
+        Vector2 perpCam = new Vector2(camForwardXZVec.y, -camForwardXZVec.x);
+        
+        // clamps hand poses so they are never behind the head and worst case are perpendicular
+        if (Vector2.Dot(leftHandXZVec, camForwardXZVec) < 0) // left hand is behind head
         {
-            leftHandXZVec = -leftHandXZVec;
-            rightHandXZVec = -rightHandXZVec;
+            if (Vector2.Dot(leftHandXZVec, perpCam) < 0) leftHandXZVec = -perpCam;
+            else leftHandXZVec = perpCam;
+        }
+        
+        if (Vector2.Dot(rightHandXZVec, camForwardXZVec) < 0) // right hand is behind head
+        {
+            if (Vector2.Dot(rightHandXZVec, perpCam) < 0) rightHandXZVec = -perpCam;
+            else rightHandXZVec = perpCam;
         }
 
         Vector2 averageVec = (leftHandXZVec + rightHandXZVec + camForwardXZVec) / 3;
