0f4426f5fe9e1c8bc1a41427e91baef7f3abf655
ImranZaminAli <izaminali@gmail.com>
2023-05-05 22:09:14 +0100

    fixed swapping between tut screens

--- Assets/Scripts/Lobby/TutorialMenu.cs
+++ Assets/Scripts/Lobby/TutorialMenu.cs
@@ -5,45 +5,85 @@ using System.Linq;
 
 public class TutorialMenu : MonoBehaviour
 {
-
-    [SerializeField] private List<GameObject> tutorialScreens;
-    [SerializeField] private List<GameObject> nextButtons;
-    [SerializeField] private List<GameObject> prevButtons;
     [SerializeField] private GameObject mainMenu;
+    [Header("TutorialScreens")]
+    [SerializeField] private GameObject startFrame;
+    [SerializeField] private GameObject grappleFrame;
+    [SerializeField] private GameObject aFrame;
+    [SerializeField] private GameObject joystickFrame;
+    [SerializeField] private GameObject bFrame;
+
+    [Header("Next buttons")]
+    [SerializeField] private UIInteraction startFrameNext;
+    [SerializeField] private UIInteraction grappleFrameNext;
+    [SerializeField] private UIInteraction aFrameNext;
+    [SerializeField] private UIInteraction joystickFrameNext;
+    [SerializeField] private UIInteraction bFrameNext;
+    
+    [Header("Prev buttons")]
+    [SerializeField] private UIInteraction grappleFramePrev;
+    [SerializeField] private UIInteraction aFramePrev;
+    [SerializeField] private UIInteraction joystickFramePrev;
+    [SerializeField] private UIInteraction bFramePrev;
+
+   
     void Awake()
     {
-        for (int i = 0; i < nextButtons.Count-1; i++)
+        startFrameNext.AddCallback((RaycastHit hit, SteamInputCore.Button button) =>
         {
-            if (nextButtons[i].TryGetComponent<UIInteraction>(out UIInteraction nextInteraction))
-            {
-                nextInteraction.AddCallback((RaycastHit hit, SteamInputCore.Button button) =>
-                {
-                    tutorialScreens[i+1].SetActive(true);
-                    tutorialScreens[i].SetActive(false);
-                });
-            }
-            else
-                Debug.LogError("no interaction script");
-        }
-
-        for (int i = 0; i < prevButtons.Count; i++)
+            grappleFrame.SetActive(true);
+            startFrame.SetActive(false);
+        });
+        
+        grappleFrameNext.AddCallback((RaycastHit hit, SteamInputCore.Button button) =>
         {
-            if (prevButtons[i].TryGetComponent<UIInteraction>(out UIInteraction prevInteraction))
-            {
-                prevInteraction.AddCallback((RaycastHit hit, SteamInputCore.Button button) =>
-                {
-                    tutorialScreens[i-1].SetActive(true);
-                    tutorialScreens[i].SetActive(false);
-                });
-            }
-            else
-                Debug.LogError("no interaction script");
-        }
-
-        if (nextButtons.Last().TryGetComponent<UIInteraction>(out UIInteraction interaction))
+            aFrame.SetActive(true);
+            grappleFrame.SetActive(false);
+        });
+        
+        aFrameNext.AddCallback((RaycastHit hit, SteamInputCore.Button button) =>
         {
-            tutorialScreens.Last().SetActive(false);
+            joystickFrame.SetActive(true);
+            aFrame.SetActive(false);
+        });
+        
+        joystickFrameNext.AddCallback((RaycastHit hit, SteamInputCore.Button button) =>
+        {
+            bFrame.SetActive(true);
+            joystickFrame.SetActive(false);
+        });
+        
+        bFrameNext.AddCallback((RaycastHit hit, SteamInputCore.Button button) =>
+        {
             mainMenu.SetActive(true);
-        }
+            bFrame.SetActive(false);
+        });
+        
+        grappleFramePrev.AddCallback((RaycastHit hit, SteamInputCore.Button button) =>
+        {
+            startFrame.SetActive(true);
+            grappleFrame.SetActive(false);
+
+        });
+        
+        aFramePrev.AddCallback((RaycastHit hit, SteamInputCore.Button button) =>
+        {
+            grappleFrame.SetActive(true);
+            aFrame.SetActive(false);
+        });
+        
+        joystickFramePrev.AddCallback((RaycastHit hit, SteamInputCore.Button button) =>
+        {
+            aFrame.SetActive(true);
+            joystickFrame.SetActive(false);
+        });
+        
+        bFramePrev.AddCallback((RaycastHit hit, SteamInputCore.Button button) =>
+        {
+            joystickFrame.SetActive(true);
+            bFrame.SetActive(false);
+        });
+        
+        
     }
 }
