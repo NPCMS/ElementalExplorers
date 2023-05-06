996e03bc7b6f03eb43d01a04a840b8afc3b82a8e
ImranZaminAli <izaminali@gmail.com>
2023-05-06 15:05:21 +0100

    fade when you swap audio

--- Assets/Scripts/Lobby/SpeakerController.cs
+++ Assets/Scripts/Lobby/SpeakerController.cs
@@ -2,12 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpeakerController : MonoBehaviour
 {
     [SerializeField] private List<AudioNamePair> voiceLines;
     [SerializeReference] private AudioSource speaker;
+    
+    [Header("Music")]
+    [SerializeReference] private AudioSource music;
+    [SerializeField] private AudioClip raceMusic;
+    [SerializeField] private AudioClip minigameMusic;
+    [SerializeField] private float fadeDuration;
+    [SerializeField] [Range(0f, 1f)] private float maxVolume;
 
     public static SpeakerController speakerController;
 
@@ -17,6 +25,47 @@ public class SpeakerController : MonoBehaviour
     {
         speakerController = this;
         DontDestroyOnLoad(this);
+        music.loop = true;
+    }
+
+    public void PlayRaceMusic()
+    {
+        // if nothing is playing
+        if (music.isPlaying != raceMusic && music.isPlaying != minigameMusic)
+        {
+            music.clip = raceMusic;
+            music.Play();
+        }
+        else
+            StartCoroutine(SwapTracks(raceMusic));
+
+    }
+
+    public void PlayMinigameMusic()
+    {
+        StartCoroutine(SwapTracks(minigameMusic));
+    }
+
+    private IEnumerator SwapTracks(AudioClip clip)
+    {
+        float time = 0;
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            music.volume = Mathf.Lerp(music.volume, 0, time / fadeDuration);
+            yield return null;
+        }
+
+        music.clip = minigameMusic;
+        music.Play();
+        time = 0;
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            music.volume = Mathf.Lerp(music.volume, maxVolume, time / fadeDuration);
+            yield return null;
+        }
+        yield break;
     }
 
     [Serializable]
