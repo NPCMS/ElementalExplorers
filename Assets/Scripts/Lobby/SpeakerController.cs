3f92f48a29224f15f91c123c8e81d01e03a2a088
Matthew Swann <cv20549@bristol.ac.uk>
2023-05-06 11:36:22 +0100

    Reworked speaker controller

--- Assets/Scripts/Lobby/SpeakerController.cs
+++ Assets/Scripts/Lobby/SpeakerController.cs
@@ -1,13 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SpeakerController : MonoBehaviour
 {
     [SerializeField] private List<AudioNamePair> voiceLines;
-    [SerializeReference] private List<AudioSource> speakers;
-    
+    [SerializeReference] private AudioSource speaker;
+
+    public static SpeakerController speakerController;
+
+    private void Start()
+    {
+        speakerController = this;
+        DontDestroyOnLoad(this);
+    }
+
     [Serializable]
     private struct AudioNamePair
     {
@@ -18,17 +27,12 @@ public class SpeakerController : MonoBehaviour
     public IEnumerator PlayAudio(string clipName)
     {
         // If the first speaker is playing audio then wait until it has finished
-        yield return new WaitWhile(() => speakers[0].isPlaying);
-        
-        foreach (AudioNamePair audioNamePair in voiceLines)
+        yield return new WaitWhile(() => speaker.isPlaying);
+
+        foreach (var audioNamePair in voiceLines.Where(audioNamePair => clipName == audioNamePair.name))
         {
-            if (clipName == audioNamePair.name)
-            {
-                foreach (AudioSource speaker in speakers)
-                {
-                    speaker.PlayOneShot(audioNamePair.clip);
-                }
-            }
+            speaker.PlayOneShot(audioNamePair.clip);
+            yield break;
         }
     }
 }
