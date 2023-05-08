61848e5e1793ea807c250ca9a9232eb77fcc32fb
Matthew Swann <cv20549@bristol.ac.uk>
2023-05-08 19:47:23 +0100

    Fixed bug

--- Assets/Scripts/Minigames/TargetScript.cs
+++ Assets/Scripts/Minigames/TargetScript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class TargetScript : NetworkBehaviour
 {
     [SerializeField] private bool isP1;
-    //[SerializeField] public Animator reverseTarget;
 
     [SerializeReference] private MeshRenderer flower;
     [SerializeReference] private MeshRenderer stem;
@@ -53,7 +52,6 @@ public class TargetScript : NetworkBehaviour
         if (dieAnimationPlayed) yield break;
         dieAnimationPlayed = true;
         
-        reverseTarget.SetBool(Reverse, true);
         //yield return new WaitForSeconds(0.15f);
         stem.enabled = false;
         flower.enabled = false;
